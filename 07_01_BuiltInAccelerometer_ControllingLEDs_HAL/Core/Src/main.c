22676a7d30ca099f113e6068fc7771e1398bc755
GameZebra <mr.xmitkotox@gmail.com>
2025-02-24 16:32:15 +0200

    reading acceleration

--- 07_01_BuiltInAccelerometer_ControllingLEDs_HAL/Core/Src/main.c
+++ 07_01_BuiltInAccelerometer_ControllingLEDs_HAL/Core/Src/main.c
@@ -48,15 +48,16 @@ SPI_HandleTypeDef hspi1;
 uint8_t reg = 0x0F | 0x80;  // WHO_AM_I register (0x0F) with read bit (0x80)
 uint8_t id = 0x0;
 uint8_t accReg [6] = {0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D};
-uint16_t accX = 0;
-uint16_t accY = 0;
-uint16_t accZ = 0;
+int16_t accX = 0;
+int16_t accY = 0;
+int16_t accZ = 0;
 
 uint8_t isEnabled = 0;
 uint8_t enableAcc[2] = {0x20, 0x67};
 uint8_t enableDRY[2] = {0x23, 0xC8};
 uint8_t status = 0;
 uint8_t data[6];
+uint8_t const range = 2;
 
 /* USER CODE END PV */
 
@@ -147,22 +148,27 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	//msg = 0x27 & 0x3F;
-	//ACCEL_CS_LOW();
-	//HAL_SPI_Transmit(&hspi1, &msg, 1, 10);
-	//HAL_SPI_Receive(&hspi1, &status, 1, 10);
-	//ACCEL_CS_HIGH();
+	msg = 0x27 & 0x3F;
+	ACCEL_CS_LOW();
+	HAL_SPI_Transmit(&hspi1, &msg, 1, 10);
+	HAL_SPI_Receive(&hspi1, &status, 1, 10);
+	ACCEL_CS_HIGH();
+	HAL_Delay(10);
+	if ((status & 0x4) != 0x4){
+		continue;
+	}
 	for(short i = 0; i<6; i++){
 		msg = accReg[i] | 0x80;
 		ACCEL_CS_LOW();                  // Enable SPI communication
 		HAL_SPI_Transmit(&hspi1, &msg, 1, HAL_MAX_DELAY);  // Send register address
 		HAL_SPI_Receive(&hspi1, &data[i], 1, HAL_MAX_DELAY);    // Receive the ID
 		ACCEL_CS_HIGH();                 // Disable SPI
+		HAL_Delay(10);
 	}
 
-	accX = data[0] << 8 | (data[1]);
-	accY = data[2] << 8 | (data[3]);
-	accZ = data[4] << 8 | (data[5]);
+	accX = data[0] | (data[1] << 8);
+	accY = data[2] | (data[3] << 8);
+	accZ = data[4] | (data[5] << 8);
 
 	HAL_Delay(10);
 
